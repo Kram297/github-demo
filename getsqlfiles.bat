0683f3b5b4faf55cb77c34d310b256155101cc8c
Mark Lanman <Mark.Lanman@Thomsonreuters.com>
2016-04-01 14:45:30 -0500

    version 1.6

---
+++ getsqlfiles.bat
@@ -0,0 +1,20 @@
+echo off
+
+if %1.==. goto SYNTAX
+if %2.==. goto SYNTAX
+
+:GETFILES
+set ver1=%1
+set ver2=%2
+set outputfile=sqlfiles_%ver1%_%ver2%.txt
+
+git diff --oneline --name-only %ver1%..%ver2% -- *.sql > %outputfile%
+pause
+@powershell -command "(gc ./%outputfile%).replace('.sql', '.sql') | Set-content ./%outputfile%
+
+GOTO END
+
+:SYNTAX
+echo getsqlfiles {tag 1} {tag 2}
+
+:END
